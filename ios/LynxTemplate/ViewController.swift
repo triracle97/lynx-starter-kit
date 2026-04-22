e5d9fffdd08028319985dbd02eb50dbe7797f4d4
Triracle97 <triracle@gmail.com>
2026-04-22 22:01:16 +0700

    feat(ios): ViewController with dev/release URL switch

---
+++ ios/LynxTemplate/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Lynx
+
+final class ViewController: UIViewController {
+  // Physical device dev: change "localhost" to the IP printed by `pnpm dev:ip`.
+  private static let devHost = "localhost"
+  private static let devPort = 3000
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+
+    #if DEBUG
+    let url = "http://\(Self.devHost):\(Self.devPort)/main.lynx.bundle"
+    #else
+    let url = "embedded://main.lynx.bundle"
+    #endif
+
+    let lynxView = LynxViewBuilder()
+      .setTemplateProvider(TemplateProvider())
+      .build()
+    lynxView.frame = view.bounds
+    lynxView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    view.addSubview(lynxView)
+    lynxView.loadTemplate(fromURL: url, initData: nil)
+  }
+}
