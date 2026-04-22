6762425e7701745ae353d1d68b0cddbc239388d4
Triracle97 <triracle@gmail.com>
2026-04-22 19:16:23 +0700

    chore: add eslint (flat config) + prettier
    Plan called for .eslintrc.cjs but ESLint 9 requires flat config.
    Using eslint.config.js; added globals dep; lint script now 'eslint .'.
    Downstream tasks expect this form.

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+
+export default [
+  { ignores: ['dist/**', 'node_modules/**', 'ios/**', 'android/**', 'pnpm-lock.yaml'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: { '@typescript-eslint': tsPlugin, react, 'react-hooks': reactHooks },
+    settings: { react: { version: '18' } },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['scripts/**/*.cjs'],
+    languageOptions: { globals: globals.node, sourceType: 'commonjs' },
+  },
+]
